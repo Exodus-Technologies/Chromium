2aaf640a9279164661986c13efb0d7c00c200924
Dwain Robinson <robinson.dwain.developer@gmail.com>
2023-08-23 22:04:19 -0500

    Updating service to upload issues without ACLs

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,6 @@ export const DEFAULT_PDF_FILE_EXTENTION = 'pdf';
 
 export const ISSUE_MIME_TYPE = 'application/pdf';
 
-export const DEFAULT_SUBSCRIPTION_TYPE = 'issue';
-
 export const DEFAULT_COVERIMAGE_FILE_EXTENTION = 'jpeg';
 
 export const COVERIMAGE_MIME_TYPE = 'image/jpeg';
